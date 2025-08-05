9192d622558ae9c8b7424fc880f871dee3a4c2ce
Zachary <remscar@gmail.com>
2025-08-05 16:56:35 -0700

    Update chain.ts

--- config/zenchain-testnet/chain.ts
+++ config/zenchain-testnet/chain.ts
@@ -1,24 +1,25 @@
 import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts/index'
 
-export const FACTORY_ADDRESS = '0x77d7fa7cb837e7a1cd48918eaec6afa996586799'
+export const FACTORY_ADDRESS = '0xe75096a9377d90d702c4c689cfc09df80ec61621'
 
 export const REFERENCE_TOKEN = '0x0000000000000000000000000000000000000804'
 export const STABLE_TOKEN_PAIRS = [
-  '0xb38d321c4db0bbe09e02c99b39146aeae250fc35', // ZTC-USDC
-  '0xab935a3d43c24f4e8a9be06e538d4335e2a0aeec', // ZTC-USDT
+  '0x016A0f043d85B254203062169D6b333011261CbD', // ZTC-USDC
+  '0x29681015CC215EA5Bf42185274218e5d2AeDb50d', // ZTC-USDT
 ]
 
 // token where amounts should contribute to tracked volume and liquidity
 export const WHITELIST: string[] = [
   '0x0000000000000000000000000000000000000804', // ZTC
-  '0x95b1a313e08c577c678d6abfc229a6fa035e95db', // ETH
-  '0xdc2b839dc176f152bc74c008de8dd6475bb0d25c', // USDC
-  '0x289128c4bd8d72cad19338453cb87fae900e759d', // USDT
+  '0x89f8267e9480bbf7833d3a0cfe98bde70f96e1a0', // ETH
+  '0x44d25859f79787ff937ec1305dbf0866d6064e21', // USDC
+  '0x2a0b66deb779ef7df45b064ef5cee2b1bf6e5dd5', // USDT
+  '0xe267b9cc76a614b8e178b4552e9983d1f19ceb05', // BTC
 ]
 
 export const STABLECOINS = [
-  "0xdc2b839dc176f152bc74c008de8dd6475bb0d25c", // USDC
-  "0x289128c4bd8d72cad19338453cb87fae900e759d", // USDT
+  "0x44d25859f79787ff937ec1305dbf0866d6064e21", // USDC
+  "0x2a0b66deb779ef7df45b064ef5cee2b1bf6e5dd5", // USDT
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
